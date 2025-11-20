189ff2da5f259942cddd799aafe77abde5d96b66
Mark John Lloyd Ncinas <jeyelncinas@gmail.com>
2025-11-20 18:23:05 +0800

    Delete SemestralBreakdown/src directory

--- SemestralBreakdown/src/game/SemestralBreakdown.java
+++
@@ -1,7 +0,0 @@
-package game;
-
-public class SemestralBreakdown {
-    public static void main(String[] args) {
-        new Game().start();
-    }
-}
--- SemestralBreakdown/src/game/events/Event.java
+++
@@ -1,8 +0,0 @@
-package game.events;
-
-import game.Player;
-
-public abstract class Event {
-    public abstract String getName();
-    public abstract void apply(Player player);
-}
